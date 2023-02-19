3389c161a8aaf610b7b9756ce19e2da1434af521
Konstantin533 <petya.didi@mail.ru>
2023-02-19 14:02:20 +0300

    Update CodeFile1.cs

--- CodeFile1.cs
+++ CodeFile1.cs
@@ -14,27 +14,27 @@ class Program
 
     {
         //Задание 1.
-        //Phone phoneOne = new Phone(78999019909, "Nokia", 5);
-        //Phone phoneTwo = new Phone(79019992323, "Samsung");
-        //Phone phoneThree = new Phone();
-        //phoneOne.ReceiveCall("Сергей");
-        //phoneTwo.ReceiveCall("Артём");
-        //phoneThree.ReceiveCall("Илья");
-        //Console.WriteLine(phoneOne.GetNumber());
-        //phoneTwo.GetNumber();
-        //phoneThree.GetNumber(79919231212);
-        //phoneOne.ReceiveCall("Дмитрий", 79889011717);
-        //phoneTwo.ReceiveCall();
-        //phoneThree.ReceiveCall();
-        //phoneOne.SendMessage(79889011717, 79919231212, 78005553535, 79099776523);
+        Phone phoneOne = new Phone(78999019909, "Nokia", 5);
+        Phone phoneTwo = new Phone(79019992323, "Samsung");
+        Phone phoneThree = new Phone();
+        phoneOne.ReceiveCall("Сергей");
+        phoneTwo.ReceiveCall("Артём");
+        phoneThree.ReceiveCall("Илья");
+        Console.WriteLine(phoneOne.GetNumber());
+        phoneTwo.GetNumber();
+        phoneThree.GetNumber(79919231212);
+        phoneOne.ReceiveCall("Дмитрий", 79889011717);
+        phoneTwo.ReceiveCall();
+        phoneThree.ReceiveCall();
+        phoneOne.SendMessage(79889011717, 79919231212, 78005553535, 79099776523);
 
 
 
         ////Задание 2.
-        //Patient patient = new Patient();
-        //patient.Plan(1);
-        
-      
+        Patient patient = new Patient();
+        patient.Plan(1);
+
+
         //Задание 3.
         CreditCard creditCard = new CreditCard(1232133212,45000);
         CreditCard creditCardOne = new CreditCard(69696969696, 25000);
